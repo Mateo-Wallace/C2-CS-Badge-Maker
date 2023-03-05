5194ce408c0c55432b829a184c61fc2df500b9b9
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-03-05 15:16:49 -0500

    starter code

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic; // allows for using dictionaries and lists
+
+// to run the file use `dotnet run`
+
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+        static void Main(string[] args) // entry point
+        {
+            List<string> employees = new List<string>() { "adam", "amy" };
+
+            employees.Add("barbara");
+            employees.Add("billy");
+
+            for (int i = 0; i < employees.Count; i++)
+            {
+                Console.WriteLine(employees[i]);
+            }
+
+        }
+    }
+}
