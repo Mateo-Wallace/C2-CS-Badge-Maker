4b939db9d3b6bcb463de4055c73153b3f5e9c6a1
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-03-05 15:37:20 -0500

    removed printemployees function from main

--- Program.cs
+++ Program.cs
@@ -9,33 +9,37 @@ namespace CatWorx.BadgeMaker
 {
     class Program
     {
-        static void Main(string[] args)
+        static List<string> GetEmployees()
         {
-            // This is our employee-getting code now
-            List<string> employees = GetEmployees();
+            List<string> employees = new List<string>();
+            while (true)
+            {
 
+                Console.WriteLine("Please enter a name: (leave empty to exit): ");
+
+                string input = Console.ReadLine() ?? "";
+
+                if (input == "")
+                {
+                    break;
+                }
+                employees.Add(input);
+            }
+            return employees;
+        }
+
+        static void PrintEmployees(List<string> employees)
+        {
             for (int i = 0; i < employees.Count; i++)
             {
                 Console.WriteLine(employees[i]);
             }
         }
 
-        static List<string> GetEmployees()
+        static void Main(string[] args)
         {
-            List<string> employees = new List<string>();
-            while (true)
-            {
-                Console.WriteLine("Please enter a name: (leave empty to exit): ");
-                string input = Console.ReadLine() ?? "";
-                if (input == "")
-                {
-                    break;
-                }
-
-                employees.Add(input);
-            }
-            // This is important!
-            return employees;
+            List<string> employees = GetEmployees();
+            PrintEmployees(employees);
         }
     }
 }
