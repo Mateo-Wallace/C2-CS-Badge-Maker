276fabeecdcf77dd3cee10eb9c5b94b7dadf4c65
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-03-05 15:31:05 -0500

    removed getemployees from main and made it its own function

--- Program.cs
+++ Program.cs
@@ -9,26 +9,33 @@ namespace CatWorx.BadgeMaker
 {
     class Program
     {
-        static void Main(string[] args) // entry point
+        static void Main(string[] args)
         {
-            List<string> employees = new List<string>();
-            // Collect user values until the value is an empty string
-            while (true)
-            {
-                Console.WriteLine("Please enter a name: (leave empty to exit): ");
-                string input = Console.ReadLine() ?? "";
-                // Break if the user hits ENTER without typing a name
-                if (input == "")
-                {
-                    break;
-                }
-                employees.Add(input);
-            }
+            // This is our employee-getting code now
+            List<string> employees = GetEmployees();
+
             for (int i = 0; i < employees.Count; i++)
             {
                 Console.WriteLine(employees[i]);
             }
+        }
 
+        static List<string> GetEmployees()
+        {
+            List<string> employees = new List<string>();
+            while (true)
+            {
+                Console.WriteLine("Please enter a name: (leave empty to exit): ");
+                string input = Console.ReadLine() ?? "";
+                if (input == "")
+                {
+                    break;
+                }
+
+                employees.Add(input);
+            }
+            // This is important!
+            return employees;
         }
     }
 }
