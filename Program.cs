d2e8e44c8859403cdf7d06ab8f85532344ee90e0
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-03-05 15:41:50 -0500

    created an employee class, called it within program

--- Program.cs
+++ Program.cs
@@ -14,15 +14,15 @@ namespace CatWorx.BadgeMaker
             List<string> employees = new List<string>();
             while (true)
             {
-
                 Console.WriteLine("Please enter a name: (leave empty to exit): ");
-
                 string input = Console.ReadLine() ?? "";
-
                 if (input == "")
                 {
                     break;
                 }
+
+                // Create a new Employee instance
+                Employee currentEmployee = new Employee();
                 employees.Add(input);
             }
             return employees;
