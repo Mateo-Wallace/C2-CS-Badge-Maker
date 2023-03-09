adaf12d608530bf90ff3b95ed512c84028abdbad
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-03-08 23:04:36 -0500

    moved get employees from program to people fetcher

---
+++ PeopleFetcher.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+    class PeopleFetcher
+    {
+        // code from GetEmployees() in Program.cs
+        public static List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            while (true)
+            {
+                // Move the initial prompt inside the loop, so it repeats for each employee
+                Console.WriteLine("Enter first name (leave empty to exit): ");
+
+                // change input to firstName
+                string firstName = Console.ReadLine() ?? "";
+                if (firstName == "")
+                {
+                    break;
+                }
+
+                // add a Console.ReadLine() for each value
+                Console.Write("Enter last name: ");
+                string lastName = Console.ReadLine() ?? "";
+                Console.Write("Enter ID: ");
+                int id = Int32.Parse(Console.ReadLine() ?? "");
+                Console.Write("Enter Photo URL:");
+                string photoUrl = Console.ReadLine() ?? "";
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+                employees.Add(currentEmployee);
+            }
+
+            return employees;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,39 +4,9 @@ namespace CatWorx.BadgeMaker
 {
     class Program
     {
-        // Update the method return type
-        static List<Employee> GetEmployees()
-        {
-            List<Employee> employees = new List<Employee>();
-            while (true)
-            {
-                // Move the initial prompt inside the loop, so it repeats for each employee
-                Console.WriteLine("Enter first name (leave empty to exit): ");
-
-                // change input to firstName
-                string firstName = Console.ReadLine() ?? "";
-                if (firstName == "")
-                {
-                    break;
-                }
-
-                // add a Console.ReadLine() for each value
-                Console.Write("Enter last name: ");
-                string lastName = Console.ReadLine() ?? "";
-                Console.Write("Enter ID: ");
-                int id = Int32.Parse(Console.ReadLine() ?? "");
-                Console.Write("Enter Photo URL:");
-                string photoUrl = Console.ReadLine() ?? "";
-                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
-                employees.Add(currentEmployee);
-            }
-
-            return employees;
-        }
-
         async static Task Main(string[] args)
         {
-            List<Employee> employees = GetEmployees();
+            List<Employee> employees = PeopleFetcher.GetEmployees();
             Util.PrintEmployees(employees);
             Util.MakeCSV(employees);
             await Util.MakeBadges(employees);
