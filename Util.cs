6330167cae11f6bb4fa15eb50c0437383777ee7c
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-03-08 22:26:28 -0500

    multiple badges generating, with name photo id# bizName

--- Util.cs
+++ Util.cs
@@ -56,6 +56,10 @@ namespace CatWorx.BadgeMaker
 
             int COMPANY_NAME_Y = 150;
 
+            int EMPLOYEE_NAME_Y = 600;
+
+            int EMPLOYEE_ID_Y = 730;
+
             using (HttpClient client = new HttpClient())
             {
                 for (int i = 0; i < employees.Count; i++)
@@ -80,9 +84,21 @@ namespace CatWorx.BadgeMaker
                     // Company name
                     canvas.DrawText(employees[i].GetCompanyName(), BADGE_WIDTH / 2f, COMPANY_NAME_Y, paint);
 
+                    paint.Color = SKColors.Black;
+
+                    // Employee name
+                    canvas.DrawText(employees[i].GetFullName(), BADGE_WIDTH / 2f, EMPLOYEE_NAME_Y, paint);
+
+                    paint.Typeface = SKTypeface.FromFamilyName("Courier New");
+
+                    // Employee ID
+                    canvas.DrawText(employees[i].GetId().ToString(), BADGE_WIDTH / 2f, EMPLOYEE_ID_Y, paint);
+
                     SKImage finalImage = SKImage.FromBitmap(badge);
                     SKData data = finalImage.Encode();
-                    data.SaveTo(File.OpenWrite("data/employeeBadge.png"));
+
+                    string template = "data/{0}_badge.png";
+                    data.SaveTo(File.OpenWrite(string.Format(template, employees[i].GetId())));
                 }
             }
         }
